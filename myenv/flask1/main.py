5ff528212e9a77167cacdc2f433bbb5cfed49f0c
Seijun2023 <sofiaarturogomez@gmail.com>
2024-01-07 22:40:50 -0500

    debuging error solved

--- myenv/flask1/main.py
+++ myenv/flask1/main.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World Flask :D'
+    return 'Hello World Flask :D, ARE YOU SEEING THIS?'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
