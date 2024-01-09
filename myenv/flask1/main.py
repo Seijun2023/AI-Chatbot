d571e73b094efbe69b1e4e225d8f80eeac3f3635
Seijun2023 <sofiaarturogomez@gmail.com>
2024-01-09 18:55:14 -0500

    add launching from main.py

--- myenv/flask1/main.py
+++ myenv/flask1/main.py
@@ -1,8 +1,9 @@
 from flask import Flask
 
+#This is the principal module
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # if / is the main page 5000, however if I put main, it will show the the same url but with 5000/main
 def hello():
     return 'Hello World Flask :D, ARE YOU SEEING THIS?'
 
